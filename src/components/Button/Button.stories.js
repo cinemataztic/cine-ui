3009e915ae0bbc69bcacf097c4da6ff0e004549d
sandhana14 <sandhana@cinemataztic.com>
2021-12-23 12:13:26 +0100

    Storybook set-up

---
+++ src/components/Button/Button.stories.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import Button from './Button.component';
+
+export default {
+  title: 'Example/Button',
+  component: Button,
+  argTypes: {
+    label: {
+      control: { type: 'text' },
+    },
+    backgroundColor: { control: { type: 'color' } },
+    color: { control: { type: 'color' } },
+    size: {
+      control: { type: 'radio', options: ['xm', 'sm', 'md', 'lg'] },
+    },
+  },
+};
+
+const Template = ({ label, backgroundColor, color, size, onClick }) => {
+  return (
+    <Button
+      label={label}
+      backgroundColor={backgroundColor}
+      color={color}
+      size={size}
+      onClick={onClick}
+      disabled={false}
+    />
+  );
+};
+
+export const ButtonComponent = Template.bind({});
+
+ButtonComponent.args = {
+  backgroundColor: 'bg-primary',
+  label: 'Click me',
+  color: 'text-textgray',
+  size: 'sm',
+  onClick: () => {},
+};
