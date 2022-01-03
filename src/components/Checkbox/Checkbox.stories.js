65b4c73b24b81dad05b35c17a6c13bcc373b66ca
sandhana14 <sandhana@cinemataztic.com>
2022-01-03 12:22:02 +0100

    Made changes based on the feedback

---
+++ src/components/Checkbox/Checkbox.stories.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import Checkbox from './Checkbox.component';
+import './Checkbox.styles.css';
+
+export default {
+  title: 'Example/Checkbox',
+  component: Checkbox,
+};
+
+const Template = () => {
+  const data = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+  const [day, setDay] = useState({
+    Mon: false,
+    Tue: false,
+    Wed: false,
+    Thu: false,
+    Fri: false,
+    Sat: false,
+    Sun: false,
+  });
+
+  const onChangeHandle = (e) => {
+    const { value, checked } = e.target;
+
+    setDay((e) => {
+      const selectedDay = e;
+      selectedDay[value] = checked;
+      return { ...selectedDay };
+    });
+  };
+
+  return (
+    <>
+      <div
+        className={`grid grid-cols-5 gap-y-6 w-4/6 h-28 px-4 border-0 rounded-lg py-6 bg-secondary bg-opacity-70 text-white`}
+      >
+        {data.map((item, index) => {
+          return (
+            <Checkbox
+              item={item}
+              key={index}
+              selectedDays={day}
+              onChange={onChangeHandle}
+              checkedBgColor={'bg-primary'}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export const CheckboxComponent = Template.bind({});
