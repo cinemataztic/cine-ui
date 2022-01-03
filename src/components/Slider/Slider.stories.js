54c592a0b7b13f53a923a80c20040492027f4aaf
sandhana14 <sandhana@cinemataztic.com>
2022-01-03 20:29:38 +0100

    Slider component

---
+++ src/components/Slider/Slider.stories.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import RangeSlider from './Slider.component';
+
+export default {
+  title: 'Example/Slider',
+  component: RangeSlider,
+};
+
+const Template = () => {
+  const [range, setRange] = useState(1000);
+
+  const onChangeSliderHandle = (e) => {
+    const { value } = e.target;
+    setRange(value);
+  };
+
+  return (
+    <>
+      <RangeSlider
+        range={range}
+        minValue={1}
+        maxValue={5000}
+        onChange={onChangeSliderHandle}
+      />
+    </>
+  );
+};
+
+export const SliderComponent = Template.bind({});
