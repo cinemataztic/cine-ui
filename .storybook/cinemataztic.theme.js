0f3d8e0f89aff5f6da40f94e106dd1197c25d09b
Allan Koudal <allan@cinemataztic.com>
2022-01-06 16:49:02 +0100

    Customize storybook theme.

---
+++ .storybook/cinemataztic.theme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'dark',
+  brandTitle: 'Cinemataztic',
+  brandUrl: 'https://www.cinemataztic.com',
+  brandImage:
+    'https://storage.googleapis.com/cinemataztic-public/cinemataztic/cinemataztic-logo-full-350x32.png',
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,7 @@
+import { addons } from '@storybook/addons';
+
+import cinemataztic from './cinemataztic.theme';
+
+addons.setConfig({
+  theme: cinemataztic,
+});
