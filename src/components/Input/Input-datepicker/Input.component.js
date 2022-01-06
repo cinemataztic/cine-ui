dffa19c62f837e875428a98dabd81047ac57408e
Allan Koudal <allan@cinemataztic.com>
2022-01-06 18:16:14 +0100

    Custom next/prev arrows in datepicker.

--- src/components/Input/Input-datepicker/Input.component.js
+++ src/components/Input/Input-datepicker/Input.component.js
@@ -26,6 +26,36 @@ const InputField = ({ value, onChange, placeholder, id }) => {
       noBorder
       showDefaultInputIcon={true}
       inputIconPosition="after"
+      navPrev={
+        <div className="DayPickerNavigation_button DayPickerNavigation_button_1 DayPickerNavigation_button__default DayPickerNavigation_button__default_2 DayPickerNavigation_button__horizontal DayPickerNavigation_button__horizontal_3 DayPickerNavigation_button__horizontalDefault DayPickerNavigation_button__horizontalDefault_4 DayPickerNavigation_leftButton__horizontalDefault DayPickerNavigation_leftButton__horizontalDefault_5">
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M13.9799 18L15.3899 16.59L10.8099 12L15.3899 7.41L13.9799 6L7.97989 12L13.9799 18Z"
+              fill="#3AC1C8"
+            />
+          </svg>
+        </div>
+      }
+      navNext={
+        <div className="DayPickerNavigation_button DayPickerNavigation_button_1 DayPickerNavigation_button__default DayPickerNavigation_button__default_2 DayPickerNavigation_button__horizontal DayPickerNavigation_button__horizontal_3 DayPickerNavigation_button__horizontalDefault DayPickerNavigation_button__horizontalDefault_4 DayPickerNavigation_rightButton__horizontalDefault DayPickerNavigation_rightButton__horizontalDefault_5">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 0 24 24"
+            width="24px"
+            fill="#3AC1C8"
+          >
+            <path d="M0 0h24v24H0V0z" fill="none" />
+            <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z" />
+          </svg>
+        </div>
+      }
       customInputIcon={
         <svg
           width="20"
