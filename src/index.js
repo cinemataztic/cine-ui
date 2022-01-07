a2c8e149c7f43d91fd12fcec0850ed5618a88ddb
Allan Koudal <allan@cinemataztic.com>
2022-01-07 11:57:19 +0100

    Fix casing on Input import.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export { default as Button } from './components/Button/Button.component';
 export { default as Checkbox } from './components/Checkbox/Checkbox.component';
 export { default as Disclosure } from './components/Disclosure/Disclosure.component';
-export { default as DateInput } from './components/input/Input-datepicker/Input.component';
+export { default as DateInput } from './components/Input/Input-datepicker/Input.component';
 export { default as Input } from './components/Input/Input-text/Input.component';
 export { default as Listbox } from './components/Listbox/Listbox.component';
 export { default as RadioGroup } from './components/RadioGroup/RadioGroup.component';
