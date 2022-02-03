ab566086e4ae297c4e9743c9cbd272181a884d1d
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2022-02-03 11:30:45 +0100

    Add accepted file type and rejection handling

--- src/components/FileUpload/FileUpload.component.js
+++ src/components/FileUpload/FileUpload.component.js
@@ -4,7 +4,7 @@ import './FileUpload.styles.css'
 import PropTypes from 'prop-types';
 
 
-const FileUpload = ({ onDrop, text='Upload File', dragActiveText='Upload File' }) => {
+const FileUpload = ({ onDrop, text='Upload File', dragActiveText='Upload File', dragRejectText='Invalid File', acceptedFileTypes }) => {
   const handleDrop = useCallback(acceptedFiles => {
     if (!onDrop) {
       console.warn('FileUpload: onDrop is not defined')
@@ -13,17 +13,28 @@ const FileUpload = ({ onDrop, text='Upload File', dragActiveText='Upload File' }
     onDrop(acceptedFiles)
   }, [])
 
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop: handleDrop})
+  const {getRootProps, getInputProps, isDragActive, isDragReject} = useDropzone({
+    onDrop: handleDrop,
+    accept: acceptedFileTypes
+  })
+
+  const DropzoneContent = () => {
+    if (isDragReject) {
+      return <div>{dragRejectText}</div>
+    }
+
+    if (isDragActive) {
+      return <div>{dragActiveText}</div>
+    }
+
+    return <div>{text}</div>
+  }
 
   return (
-    <div {...getRootProps()} className={`w-full border-2 border-dashed border-gray-300 rounded-lg p-4 transition-opacity duration-150 ease-in-out bg-gray-700 ${isDragActive ? 'opacity-80' : ''}`}>
+    <div {...getRootProps()} className={`w-full border-2 border-dashed  ${isDragReject ? 'border-red-300' : 'border-gray-300'} rounded-lg p-4 transition-opacity duration-150 ease-in-out ${isDragActive ? 'opacity-80' : ''}`}>
       <input {...getInputProps()} />
-      <div className='flex justify-center items-center text-gray-300'>
-        {
-          isDragActive ?
-            <div>{dragActiveText}</div> :
-            <div>{text}</div>
-        }
+      <div className={`flex justify-center items-center ${isDragReject ? 'text-red-300' : 'text-gray-300'}`}>
+        <DropzoneContent />
       </div>
     </div>
   )
@@ -33,11 +44,13 @@ FileUpload.PropTypes = {
   onDrop: PropTypes.func.isRequired,
   text: PropTypes.node,
   dragActiveText: PropTypes.node,
+  acceptedFileTypes: PropTypes.string
 }
 
 FileUpload.defaultProps = {
   text: 'Upload File',
-  dragActiveText: 'Upload File'
+  dragActiveText: 'Upload File',
+  dragRejectText: 'Invalid File',
 }
 
 export default FileUpload;
