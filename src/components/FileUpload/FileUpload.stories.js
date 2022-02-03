fb8bf2cef2e27a5a4558f33187f752fb0fd1f660
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2022-02-03 11:30:59 +0100

    Add "only images" example

--- src/components/FileUpload/FileUpload.stories.js
+++ src/components/FileUpload/FileUpload.stories.js
@@ -6,20 +6,33 @@ export default {
   component: FileUpload,
 };
 
-const Template = ({ onDrop, text, dragActiveText }) => {
+const Template = ({ onDrop, text, dragActiveText, dragRejectText, acceptedFileTypes }) => {
   return (
     <FileUpload
       onDrop={onDrop}
       text={text}
       dragActiveText={dragActiveText}
+      acceptedFileTypes={acceptedFileTypes}
+      dragRejectText={dragRejectText}
     />
   );
 };
 
+
+// Base file upload example
 export const Base = Template.bind({});
 
 Base.args = {
   onDrop: (acceptedFiles) => {alert(acceptedFiles.map(file => file.name))},
-  text: 'Click or drag file here',
-  dragActiveText: 'Drop file here'
+};
+
+
+// File upload that only accepts images
+export const OnlyImages = Template.bind({});
+
+OnlyImages.args = {
+  onDrop: (acceptedFiles) => {alert(acceptedFiles.map(file => file.name))},
+  text: 'Upload Image (JPG, PNG)',
+  dragActiveText: 'Drop file(s) here',
+  acceptedFileTypes: 'image/jpeg, image/png'
 };
