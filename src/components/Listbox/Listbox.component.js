68cc955643992499f506d238d6362636dab46fef
Mikkel Westermann <mikkel.westermann.98@gmail.com>
2022-02-27 16:01:08 +0100

    Add loading state to listbox

--- src/components/Listbox/Listbox.component.js
+++ src/components/Listbox/Listbox.component.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Listbox as HeadlessListBox } from '@headlessui/react';
+import { Spinner } from '../..';
 
 import './Listbox.styles.css';
 
@@ -28,18 +29,28 @@ const ListBox = ({
   emptyHeader,
   emptyExplanation,
   emptyCTA,
+  loading
 }) => {
   return (
-    <HeadlessListBox as="div" value={value} onChange={onChange}>
+    <HeadlessListBox disabled={loading} as="div" value={value} onChange={onChange}>
       <HeadlessListBox.Button className="border-2 border-secondary focus:border-primary bg-tertiary text-white w-full h-12 rounded-lg text-left pl-4 ">
         <span className="flex justify-between">
           <span>{value ? getLabelForValue(value, options) : placeholder}</span>
-          <span className="mt-2 mr-4">
-            <img
-              src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIiIGhlaWdodD0iOCIgdmlld0JveD0iMCAwIDEyIDgiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0xMC41OSAwLjU5MDAyN0w2IDUuMTcwMDNMMS40MSAwLjU5MDAyN0wwIDIuMDAwMDNMNiA4LjAwMDAzTDEyIDIuMDAwMDNMMTAuNTkgMC41OTAwMjdaIiBmaWxsPSJ3aGl0ZSIvPgo8L3N2Zz4K"
-              alt="Down Arrow"
-            />
-          </span>
+          {
+            loading ?
+              <span className='mr-4'>
+                <Spinner size="small" />
+              </span>
+              :
+              (
+                <span className="mt-2 mr-4">
+                  <img
+                    src="data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIiIGhlaWdodD0iOCIgdmlld0JveD0iMCAwIDEyIDgiIGZpbGw9Im5vbmUiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CjxwYXRoIGQ9Ik0xMC41OSAwLjU5MDAyN0w2IDUuMTcwMDNMMS40MSAwLjU5MDAyN0wwIDIuMDAwMDNMNiA4LjAwMDAzTDEyIDIuMDAwMDNMMTAuNTkgMC41OTAwMjdaIiBmaWxsPSJ3aGl0ZSIvPgo8L3N2Zz4K"
+                    alt="Down Arrow"
+                  />
+                </span>
+              )
+          }
         </span>
       </HeadlessListBox.Button>
       <HeadlessListBox.Options className="border-2 border-secondary focus:border-primary bg-tertiary w-full rounded-lg max-h-80 overflow-y-auto text-white ">
