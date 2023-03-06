ed7060e00a490feca0d8b9f2c166b76c4df63f2a
sandhana14 <sandhana@cinemataztic.com>
2023-03-06 14:30:05 +0100

    Pass onBlur prop to radio group component

--- src/components/RadioGroup/RadioGroup.component.js
+++ src/components/RadioGroup/RadioGroup.component.js
@@ -4,12 +4,13 @@ import PropTypes from 'prop-types';
 
 import './RadioGroup.styles.css';
 
-const RadioGroup = ({ options, value, onChange, className }) => {
+const RadioGroup = ({ options, value, onChange, className, onBlur }) => {
   return (
     <HeadlessRadioGroup
       value={value}
       onChange={onChange}
       className={`flex flex-wrap text-white ${className}`}
+      onBlur={onBlur}
     >
       {options.map((option) => {
         return (
